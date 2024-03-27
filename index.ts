59e5ff9428f4f97655026ec5f78f1f630a29d8a3
youtgithubusername <youtgithubemail>
2024-03-27 10:56:29 +0500

    push my cli-atm

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance: number = 20000;
+let myPin: number = 67890;
+let answer = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "enter your pin code: ",
+  },
+]);
+if (answer.pin === myPin) {
+  let operations = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      choices: ["withdraw", "checkBalance"],
+      message: "select operation",
+    },
+  ]);
+  if (operations.operation === "withdraw") {
+    let selection = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "list",
+        choices: [500, 1000, 5000, 10000, 15000, 20000, 25000, 30000],
+        message: "select amount",
+      },
+    ]);
+    if (selection.amount > myBalance) {
+      console.log("Insufficient balanace");
+    } else {
+      let remainingBalance = (myBalance -= selection.amount);
+      console.log(`your remaining balance is ${remainingBalance}.`);
+    }
+  } else if (operations.operation === "checkBalance") {
+    console.log(`your current balance is ${myBalance}.`);
+  }
+} else {
+  console.log("enter valid pin code.");
+}
